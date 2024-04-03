3550b7da6d2ad0768d96276d0d80a4caf96353df
Carolina <cisj24@gmail.com>
2024-04-03 18:04:00 +0200

    started with routes

--- models/User.model.js
+++ models/User.model.js
@@ -5,6 +5,7 @@ const userSchema = new Schema(
   {
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
     email: {
       type: String,
       required: true,
